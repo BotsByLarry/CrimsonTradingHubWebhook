59c129eec08d23f81fe6a424301597527b4b8ad0
BotsByLarry <advaitpathak27@proton.me>
2026-03-20 10:40:00 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,10 @@
+// Webhooks are injected by GitHub Actions
+// This file is never committed to git for security
 const WEBHOOKS = {
-    "announcements": "https://discord.com/api/webhooks/1483854676934918225/_2mmWLi1PuBxV58ikEXo1aupaGCWUPyYijmQqhROIvgwD8UW54xW3UTNW4cCwBDHHmH-",
-    "mod-announcements": "https://discord.com/api/webhooks/1483871790164869140/-QJ7GO4J6SsPggzmBWekYmLfRdfmA5FiWUnsmIhOdaB59OEMK6Lpn853zKGTZGQZvkl0",
-    "info": "https://discord.com/api/webhooks/1483855941349937242/gUrGal0FXsbGBggpcOuUJs1RUB9xsGtPDMqkqRD_7WzAW3eYEdHfDtDRyGvGh9ILuXMC",
-    "paid-roles": "https://discord.com/api/webhooks/1483854664150552666/niZOU25_tQZeFUBwdQCL9mtZJ9q29UlajbVGjbokzr1QuHbvmHGPITLW1bU6EdWNRzfT",
-    "rules": "https://discord.com/api/webhooks/1483857076152303667/36nAl0cUhsM5dZc0B2oqPTcRFW2tuiYFq8cYqNfp7qzrn77nyKhkwYxVek8j0oBPcJr",
-    "request-middleman": "https://discord.com/api/webhooks/1483857172382355667/MiSm48_r0l9U1BU8HVk3XBRGHlBxorVSJOhMCCEJxBc7aiXB58atEukdKGzEUg70ldHx",
+    "announcements": "",
+    "mod-announcements": "",
+    "info": "",
+    "paid-roles": "",
+    "rules": "",
+    "request-middleman": "",
 };
